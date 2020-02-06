90e77749992385b66a2ce413fb13e6a858e84bc7
szpila127 <inglot.seb@gmail.com>
2020-02-06 22:52:53 +0100

    First commit

---
+++ src/main/java/com/transfer/app7/TransferApp7Application.java
@@ -0,0 +1,13 @@
+package com.transfer.app7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferApp7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/transfer/app7/TransferApp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfer.app7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferApp7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
