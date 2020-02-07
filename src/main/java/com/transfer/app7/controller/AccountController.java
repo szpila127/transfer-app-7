84cd67da9ca251311fdda71f9693c21506afe319
szpila127 <inglot.seb@gmail.com>
2020-02-07 17:17:46 +0100

    Controller

--- src/main/java/com/transfer/app7/controller/AccountController.java
+++ src/main/java/com/transfer/app7/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.transfer.app7.controller;
 
+import com.transfer.app7.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("/v1/ta7/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
 }
--- src/main/java/com/transfer/app7/controller/UserController.java
+++ src/main/java/com/transfer/app7/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.transfer.app7.controller;
 
+import com.transfer.app7.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("/v1/ta7/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 }
