f78aa3b2f7efcf2c85d6a933b145776cc69df12a
szpila127 <inglot.seb@gmail.com>
2020-02-07 19:33:03 +0100

    UserController

--- src/main/java/com/transfer/app7/controller/UserController.java
+++ src/main/java/com/transfer/app7/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.transfer.app7.controller;
 
 import com.transfer.app7.domain.UserDto;
+import com.transfer.app7.mapper.UserMapper;
 import com.transfer.app7.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,6 +19,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping
-    public List<UserDto>
+    public List<UserDto> getUsers() {
+        return userMapper.mapToUserDtoList(userService.getAllUsers());
+    }
 }
