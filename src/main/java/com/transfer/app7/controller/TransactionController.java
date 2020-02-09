ea0e3ae9221aa037c8c496495cc7a12b55bef18c
szpila127 <inglot.seb@gmail.com>
2020-02-09 23:59:26 +0100

    TransactionFacade

--- src/main/java/com/transfer/app7/controller/TransactionController.java
+++ src/main/java/com/transfer/app7/controller/TransactionController.java
@@ -2,8 +2,6 @@ package com.transfer.app7.controller;
 
 import com.transfer.app7.domain.TransactionDto;
 import com.transfer.app7.facade.TransactionFacade;
-import com.transfer.app7.mapper.TransactionMapper;
-import com.transfer.app7.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +14,6 @@ public class TransactionController {
 
     @Autowired
     private TransactionFacade transactionFacade;
-
-    @Autowired
-    private TransactionService transactionService;
-
-    @Autowired
-    private TransactionMapper transactionMapper;
 
     @PostMapping(consumes = "application/json")
     public void createTransaction(@RequestBody TransactionDto transactionDto) {
