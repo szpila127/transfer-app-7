ff5159810cfc8d547aaeaa7fafafd914196e0857
szpila127 <inglot.seb@gmail.com>
2020-02-12 00:01:26 +0100

    AspectWatcherTransactionCreate

--- src/main/java/com/transfer/app7/controller/CurrencyController.java
+++ src/main/java/com/transfer/app7/controller/CurrencyController.java
@@ -2,12 +2,10 @@ package com.transfer.app7.controller;
 
 import com.transfer.app7.client.NbpApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/v1/ta7/currency")
 public class CurrencyController {
 
--- src/main/java/com/transfer/app7/controller/EmailValidatorController.java
+++ src/main/java/com/transfer/app7/controller/EmailValidatorController.java
@@ -3,12 +3,10 @@ package com.transfer.app7.controller;
 import com.transfer.app7.client.EmailValidatorApiClient;
 import com.transfer.app7.domain.dto.emailValidator.EmailValidatorDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/v1/ta7/validate")
 public class EmailValidatorController {
 
