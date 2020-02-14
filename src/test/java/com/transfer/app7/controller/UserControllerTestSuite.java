9d5107cf7879ce4e4403b3e95034a2137a666941
szpila127 <inglot.seb@gmail.com>
2020-02-14 23:00:10 +0100

    Tests

---
+++ src/test/java/com/transfer/app7/controller/UserControllerTestSuite.java
@@ -0,0 +1,10 @@
+package com.transfer.app7.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTestSuite {
+}
