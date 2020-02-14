c26cc3b971f043f8dda38af3654c7c8473af879a
szpila127 <inglot.seb@gmail.com>
2020-02-14 23:16:18 +0100

    Tests

---
+++ src/test/java/com/transfer/app7/service/EmailServiceTestSuite.java
@@ -0,0 +1,52 @@
+package com.transfer.app7.service;
+
+import com.transfer.app7.domain.Mail;
+import com.transfer.app7.domain.dto.AppEventDto;
+import com.transfer.app7.facade.AppEventFacade;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTestSuite {
+
+    @InjectMocks
+    private EmailService emailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Mock
+    private AppEventFacade appEventFacade;
+
+    @Test
+    public void shouldSendMail() {
+        //Given
+        Mail mail = new Mail("test@test.com", "Test", "Test message");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        doNothing().when(appEventFacade).createEvent(ArgumentMatchers.any(AppEventDto.class));
+
+        //When
+        emailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+
+    @Test
+    public void testCreateMailMessage() {
+
+    }
+}
